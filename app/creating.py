43b7275397c9c7d66a67e6eb06e54a6f3748f349
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-08-01 21:51:47 +0530

    Updated the APIs for n number of params without hardcoding

--- app/creating.py
+++ app/creating.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 import pymongo
 import os
 import json
-
+from utils.utils import encode_payload
 
 
 client = pymongo.MongoClient(os.environ["MONGO_DB_URI"])
@@ -17,37 +17,30 @@ def create_campaign():
   
     json_body = request.json
     
-    print(json_body)
-    ad_account_id = json_body['ad_account_id']
-    name = json_body['name']
-    objective = json_body['objective']
-    status = json_body['status']
-    special_ad_categories = json_body['special_ad_categories']
+    params= json_body['params']
+      
     access_token = json_body['access_token']
+    ad_account_id = json_body['ad_account_id']
     api_version = json_body['api_version']
+    
+    payload=params
+    payload['access_token']=access_token
     
     collection = db["campaigns"] 
 
-    payload = {
-        'name': name,
-        'objective': objective,
-        'status': status,
-        'special_ad_categories': special_ad_categories,
-        'access_token': access_token  # Include access token in the payload
-    }
-    
     print(payload)
     url = f"https://graph.facebook.com/v{api_version}/act_{ad_account_id}/campaigns"
 
     response = requests.post(url, data=payload)
     
-    campaignId=response.json()
-    
-    print(campaignId)
-    
-    result = collection.insert_one(campaignId)
-    
-    print(result)
+    if(response.status_code==200):
+        campaignId=response.json()
+        print(campaignId)
+        saving_data=params
+        saving_data["campaignId"]=campaignId['id']
+        result = collection.insert_one(saving_data)
+        print(result)
+        
     return response.json()
 
 
@@ -58,44 +51,79 @@ def create_adset():
     
     json_body = request.json
     ad_account_id = json_body['ad_account_id']
-    name = json_body['name']
-    optimization_goal = json_body['optimization_goal']
-    billing_event = json_body['billing_event']
-    bid_amount = json_body['bid_amount']
-    daily_budget = json_body['daily_budget']
-    campaign_id = json_body['campaign_id']
-    targeting = json_body['targeting']
-    start_time = json_body['start_time']
-    status = json_body['status']
+    
+    params= json_body['params']
     access_token = json_body['access_token']
     api_version = json_body['api_version']
     
     collection = db["ad-sets"] 
 
-    if not all([ad_account_id, name, optimization_goal, billing_event, bid_amount, daily_budget, campaign_id, targeting, start_time, status, access_token, api_version]):
+    if not all([params,ad_account_id, access_token, api_version]):
         return "Missing required fields", 400
+    
+    payload= params
+    payload['access_token']=access_token
+    
+    payload=encode_payload(payload=payload)
+    
+    print(payload)
+    print(ad_account_id)
 
     url = f"https://graph.facebook.com/v{api_version}/act_{ad_account_id}/adsets"
-    payload = {
-        'name': name,
-        'optimization_goal': optimization_goal,
-        'billing_event': billing_event,
-        'bid_amount': bid_amount,
-        'daily_budget': daily_budget,
-        'campaign_id': campaign_id,
-        'targeting': json.dumps(targeting),
-        'start_time': start_time,
-        'status': status,
-        'access_token': access_token
-    }
+
     response = requests.post(url, data=payload)
     
-    adset_id=response.json()
-    result = collection.insert_one()
-    
+    if response.status_code==200:
+        adset_id=response.json()
+        adset_details=payload
+        adset_details['adset_id']=adset_id['id']
+        result = collection.insert_one(adset_details)
+        
     return jsonify(response.json())
 
 
+
+# @app.route('/create_adset', methods=['POST'])
+# def create_adset():
+    
+#     json_body = request.json
+#     ad_account_id = json_body['ad_account_id']
+#     name = json_body['name']
+#     optimization_goal = json_body['optimization_goal']
+#     billing_event = json_body['billing_event']
+#     bid_amount = json_body['bid_amount']
+#     daily_budget = json_body['daily_budget']
+#     campaign_id = json_body['campaign_id']
+#     targeting = json_body['targeting']
+#     start_time = json_body['start_time']
+#     status = json_body['status']
+#     access_token = json_body['access_token']
+#     api_version = json_body['api_version']
+    
+#     collection = db["ad-sets"] 
+
+#     if not all([ad_account_id, access_token, api_version]):
+#         return "Missing required fields", 400
+
+#     url = f"https://graph.facebook.com/v{api_version}/act_{ad_account_id}/adsets"
+#     payload = {
+#         'name': name,
+#         'optimization_goal': optimization_goal,
+#         'billing_event': billing_event,
+#         'bid_amount': bid_amount,
+#         'daily_budget': daily_budget,
+#         'campaign_id': campaign_id,
+#         'targeting': json.dumps(targeting),
+#         'start_time': start_time,
+#         'status': status,
+#         'access_token': access_token
+#     }
+#     response = requests.post(url, data=payload)
+    
+#     adset_id=response.json()
+#     result = collection.insert_one(adset_id)
+    
+#     return jsonify(response.json())
     
     
     
@@ -108,20 +136,18 @@ def create_adcreative():
     ad_account_id = json_body['ad_account_id']
     name = json_body['name']
     object_story_spec = json_body['object_story_spec']
-    degrees_of_freedom_spec = json_body['degrees_of_freedom_spec']
     access_token = json_body['access_token']
     api_version = json_body['api_version']
     
     collection = db["creatives"] 
 
-    if not all([ad_account_id, name, object_story_spec, degrees_of_freedom_spec, access_token, api_version]):
+    if not all([ad_account_id, name, object_story_spec,access_token, api_version]):
         return "Missing required fields", 400
 
     url = f"https://graph.facebook.com/v{api_version}/act_{ad_account_id}/adcreatives"
     payload = {
         'name': name,
-        'object_story_spec': object_story_spec,
-        'degrees_of_freedom_spec': json.dumps(degrees_of_freedom_spec),
+        'object_story_spec':json.dumps(object_story_spec) ,
         'access_token': access_token
     }
     
--- utils/utils.py
+++ utils/utils.py
@@ -1,13 +1,10 @@
 import requests
 from flask import current_app
+import json
 
-def create_facebook_campaign(ad_account_id, access_token, payload):
-    url = f"https://graph.facebook.com/v{current_app.config['FB_API_VERSION']}/act_{ad_account_id}/campaigns"
-
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
-
-    response = requests.post(url, data=payload, headers=headers)
-    return response.json()
+def encode_payload(payload):
+    for key, value in payload.items():
+        if isinstance(value, (dict, list)):
+            payload[key] = json.dumps(value)
+    return payload
 
